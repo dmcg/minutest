c1f30111dbf13b11a06c8d04b6397f64e5b13887
Nat Pryce <sw@natpryce.com>
2018-10-30 23:26:27 +0000

    Remove the nullable fixtureFn method from `deriveFixture` API method

--- core/src/main/kotlin/com/oneeyedmen/minutest/minutest.kt
+++ core/src/main/kotlin/com/oneeyedmen/minutest/minutest.kt
@@ -50,18 +50,14 @@ interface Context<ParentF, F> {
     /**
      * Define a sub-context, inheriting the fixture from this.
      */
-    fun context(name: String, builder: Context<F, F>.() -> Unit) {
-        derivedContext(name, { this }) {
-            builder()
-        }
-    }
+    fun context(name: String, builder: Context<F, F>.() -> Unit)
     
     /**
      * Define a sub-context with a different fixture type.
      *
      * You will have to call [fixture]' in the sub-context to convert from the parent to the child fixture type.
      */
-    fun <G> derivedContext(name: String, fixtureFn: (F.() -> G)? = null, builder: Context<F, G>.() -> Unit)
+    fun <G> derivedContext(name: String, builder: Context<F, G>.() -> Unit)
 }
 
 typealias TestContext<F> = Context<*, F>
