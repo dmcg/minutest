da436fdd3de10adb1e2abd76a07f496f4f3c4f6a
Duncan McGregor <duncan@oneeyedmen.com>
2018-11-03 22:28:02 +0000

    Tidy

--- core/src/test/kotlin/com/oneeyedmen/minutest/NullableFixtureTests.kt
+++ core/src/test/kotlin/com/oneeyedmen/minutest/NullableFixtureTests.kt
@@ -1,29 +1,23 @@
 package com.oneeyedmen.minutest
 
 import com.oneeyedmen.minutest.junit.junitTests
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNull
 import org.junit.jupiter.api.TestFactory
 
 
 object NullableFixtureTests {
 
-    @TestFactory fun nullableFixture() = junitTests<Unit> {
-
-        derivedContext<String?>("with null fixture") {
-            fixture { null }
-            test("fixture is null") {
-                assertNull(this)
-            }
+    @TestFactory fun `nullable String`() = junitTests<String?>(null) {
+        test("fixture is null") {
+            assertNull(this)
         }
+    }
 
-        derivedContext<String>("with non-null fixture") {
-            fixture { "banana" }
-            test("fixture is not null") {
-                val copy: String = this ?: "was null"
-                assertEquals("banana", copy)
+    @TestFactory fun `nullable String with late fixture`() = junitTests<String?> {
+        fixture { null }
 
-            }
+        test("fixture is null") {
+            assertNull(this)
         }
     }
 }
