bce16a43ffe2a36209749944aaea176965dea50c
Duncan McGregor <duncan@oneeyedmen.com>
2018-11-04 15:08:53 +0000

    Use fixture creation on Unit

--- core/src/main/kotlin/com/oneeyedmen/minutest/internal/creating.kt
+++ core/src/main/kotlin/com/oneeyedmen/minutest/internal/creating.kt
@@ -19,10 +19,24 @@ fun <F> topLevelContext(
     builder: Context<Unit, F>.() -> Unit
 ): Context<Unit, F> =
     ContextBuilder<Unit, F>(
-        name,
-        type,
-        fixtureFunFor(type.classifier == Unit::class),
-        false
+        name = name,
+        type = type,
+        fixtureFactory = null,
+        explicitFixtureFactory = false
+    ).apply(builder)
+
+
+fun <F> topLevelContext(
+    name: String,
+    type: KType,
+    fixtureBuilder: (Unit.() -> F)?,
+    builder: Context<Unit, F>.() -> Unit
+): Context<Unit, F> =
+    ContextBuilder(
+        name = name,
+        type = type,
+        fixtureFactory = fixtureBuilder,
+        explicitFixtureFactory = true
     ).apply(builder)
 
 fun <F> topLevelContext(
@@ -33,19 +47,8 @@ fun <F> topLevelContext(
 ): Context<Unit, F> =
     topLevelContext<F>(name, type, { fixture }, builder)
 
-fun <F> topLevelContext(
-    name: String,
-    type: KType,
-    fixtureBuilder: (Unit.() -> F)?,
-    builder: Context<Unit, F>.() -> Unit
-): Context<Unit, F> =
-    ContextBuilder(name, type, fixtureBuilder, true).apply(builder)
 
-
-@Suppress("UNCHECKED_CAST")
-private fun <F> fixtureFunFor(isUnit: Boolean): (Unit.() -> F)? = if (isUnit) {{ Unit as F }} else null
-
-fun KClass<*>.asKType(isNullable: Boolean) =  object : KType {
+fun KClass<*>.asKType(isNullable: Boolean) = object : KType {
     override val arguments: List<KTypeProjection> = emptyList()
     override val classifier: KClassifier = this@asKType
     override val isMarkedNullable = isNullable
