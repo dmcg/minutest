d5d26fc548faada75a6a20bb7ba0503ed9f6cbce
Duncan McGregor <duncan@oneeyedmen.com>
2018-11-06 23:18:03 +0000

    Reinstate repurposed test

--- core/src/test/kotlin/com/oneeyedmen/minutest/NamingTests.kt
+++ core/src/test/kotlin/com/oneeyedmen/minutest/NamingTests.kt
@@ -34,9 +34,41 @@ object NamingTests {
             log
         )
     }
-    
+
     @org.junit.jupiter.api.Test
     fun `names are passed to instrumentedFixture block`() {
+        val log = mutableListOf<List<String>>()
+
+        class Fixture(val name: List<String>)
+
+        executeTest(junitTests<Fixture> {
+
+            instrumentedFixture { _, testDescriptor ->
+                Fixture(testDescriptor.fullName())
+            }
+
+            context("outer") {
+                test("outer test") { log.add(name) }
+
+                context("inner") {
+                    test("inner test 1") { log.add(name) }
+                    test("inner test 2") { log.add(name) }
+                }
+            }
+        })
+
+        assertEquals(
+            listOf(
+                listOf(javaClass.canonicalName, "outer", "outer test"),
+                listOf(javaClass.canonicalName, "outer", "inner", "inner test 1"),
+                listOf(javaClass.canonicalName, "outer", "inner", "inner test 2")
+            ),
+            log
+        )
+    }
+
+    @org.junit.jupiter.api.Test
+    fun `names are passed to withTestDescriptor block`() {
         val log = mutableListOf<List<String>>()
 
         class Fixture(val name: List<String>)
