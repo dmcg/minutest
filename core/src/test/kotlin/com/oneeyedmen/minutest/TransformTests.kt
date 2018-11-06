703c5159d7f599683261daddf323ee610ab57676
Nat Pryce <sw@natpryce.com>
2018-11-06 08:37:05 +0000

    Test for how TestTransforms behave when specified in nested contexts
    Plus a convenience function for transforming tests giving only a
    different block to execute.

--- core/src/test/kotlin/com/oneeyedmen/minutest/TransformTests.kt
+++ core/src/test/kotlin/com/oneeyedmen/minutest/TransformTests.kt
@@ -1,48 +1,80 @@
 package com.oneeyedmen.minutest
 
 import com.oneeyedmen.minutest.junit.junitTests
-import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
 
 
 object TransformTests {
-    
-    @org.junit.jupiter.api.Test
+    @Test
     fun `transforms wrap around application of before and after blocks`() {
         val log = mutableListOf<String>()
         
         executeTest(junitTests<Unit> {
-            before {
-                log.add("before")
-            }
+            before { log.add("before") }
+            after { log.add("after") }
             
             addTransform { test ->
-                object : Test<Unit>, Named by test {
-                    override fun invoke(fixture: Unit) {
-                        log.add("entering transformed test")
-                        test(fixture)
-                        log.add("leaving transformed test")
-                    }
+                test.withAction { fixture ->
+                    log.add("entering transformed test")
+                    test(fixture)
+                    log.add("leaving transformed test")
                 }
             }
             
-            after {
-                log.add("after")
+            test("the test") { log.add("the test") }
+        })
+        
+        assertLogged(log,
+            "entering transformed test",
+            "before",
+            "the test",
+            "after",
+            "leaving transformed test"
+        )
+    }
+    
+    @Test
+    fun `transforms nest, following nesting of contexts`() {
+        val log = mutableListOf<String>()
+        
+        executeTest(junitTests<Unit> {
+            addTransform { test ->
+                test.withAction { fixture ->
+                    log.add("entering outer transformed test")
+                    test(fixture)
+                    log.add("leaving outer transformed test")
+                }
             }
             
-            test("the test") {
-                log.add("the test")
+            before { log.add("before outer") }
+            after { log.add("after outer") }
+            
+            context("inner") {
+                addTransform { test ->
+                    test.withAction { fixture ->
+                        log.add("entering inner transformed test")
+                        test(fixture)
+                        log.add("leaving inner transformed test")
+                    }
+                }
+    
+                before { log.add("before inner") }
+                after { log.add("after inner") }
+                
+                test("the test") { log.add("the test") }
             }
         })
         
-        assertEquals(
-            listOf(
-                "entering transformed test",
-                "before",
-                "the test",
-                "after",
-                "leaving transformed test"
-            ),
-            log
+        assertLogged(log,
+            "entering outer transformed test",
+            "before outer",
+            "entering inner transformed test",
+            "before inner",
+            "the test",
+            "after inner",
+            "leaving inner transformed test",
+            "after outer",
+            "leaving outer transformed test"
         )
     }
 }
