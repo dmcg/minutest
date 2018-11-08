052e1cb823336124365305e7a7f4d5d57f702930
Duncan McGregor <duncan@oneeyedmen.com>
2018-11-08 16:59:05 +0000

    Stop AssumeTests cluttering the output

--- core/src/test/kotlin/com/oneeyedmen/minutest/AssumeTests.kt
+++ core/src/test/kotlin/com/oneeyedmen/minutest/AssumeTests.kt
@@ -3,18 +3,24 @@ package com.oneeyedmen.minutest
 import com.oneeyedmen.minutest.junit.junitTests
 import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Assumptions.assumeTrue
-import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.assertThrows
+import org.opentest4j.TestAbortedException
 
 
-object AssumeTests {
+class AssumeTests {
 
-    @TestFactory fun `assume works`() = junitTests<Unit> {
+    @org.junit.jupiter.api.Test fun `assume works`() {
 
-        test("try it") {
-            @Suppress("SimplifyBooleanWithConstants")
-            assumeTrue("black" == "white")
-            fail("shouldn't get here")
-            // and, more to the point, the test runner shows a skipped test
+        val tests = junitTests<Unit> {
+            test("try it") {
+                assumeTrue("black".toLowerCase() == "white")
+                fail("shouldn't get here")
+            }
         }
+
+        assertThrows<TestAbortedException> {
+            executeTest(tests)
+        }
+
     }
 }
