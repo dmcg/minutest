aeebc0b6cea1c5e1819c353a944087e3ecd61079
Nat Pryce <sw@natpryce.com>
2018-11-22 08:49:52 +0000

    Add test of selecting Minutest tests by class

---
+++ core/src/test/kotlin/example/ExampleTypedMinutest.kt
@@ -0,0 +1,15 @@
+// explicit name for generated class so that tests are not coupled to the behaviour of the kotlin compiler
+@file:JvmName("ExplicitTypedMinutest")
+package example
+
+import com.oneeyedmen.minutest.experimental.context
+import org.junit.jupiter.api.Assertions
+import java.util.Stack
+
+val `example typed context` = context<Stack<String>> {
+    fixture { Stack() }
+    
+    test("a typed fixture test") {
+        Assertions.assertTrue(isEmpty())
+    }
+}
