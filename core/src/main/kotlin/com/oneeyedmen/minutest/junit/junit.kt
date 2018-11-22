55ee0684a19ca2367ea7032b0577f5eb54c8ab91
Duncan McGregor <duncan@oneeyedmen.com>
2018-11-22 19:15:08 +0000

    Remove junitTestsNamed - BREAKING CHANGE

--- core/src/main/kotlin/com/oneeyedmen/minutest/junit/junit.kt
+++ core/src/main/kotlin/com/oneeyedmen/minutest/junit/junit.kt
@@ -2,27 +2,10 @@ package com.oneeyedmen.minutest.junit
 
 import com.oneeyedmen.minutest.Context
 import com.oneeyedmen.minutest.internal.*
-import org.junit.jupiter.api.DynamicContainer
 import org.junit.jupiter.api.DynamicContainer.dynamicContainer
 import org.junit.jupiter.api.DynamicNode
 import org.junit.jupiter.api.DynamicTest.dynamicTest
 import java.util.stream.Stream
-
-/**
- * Define a [Context] and map it to be used as a JUnit [org.junit.jupiter.api.TestFactory].
- *
- * @see [Any.junitTests]
- */
-inline fun <reified F> junitTestsNamed(name: String,
-    noinline builder: Context<Unit, F>.() -> Unit
-): Stream<out DynamicNode> =
-    topLevelContext(name, asKType<F>(), builder).toStreamOfDynamicNodes()
-
-inline fun <reified F> junitTestsNamed(name: String,
-    fixture: F,
-    noinline builder: Context<Unit, F>.() -> Unit
-): Stream<out DynamicNode> =
-    topLevelContext(name, asKType<F>(), fixture, builder).toStreamOfDynamicNodes()
 
 
 /**
@@ -31,27 +14,20 @@ inline fun <reified F> junitTestsNamed(name: String,
  * Designed to be called inside a class and to use the name as the class as the name of the test.
  */
 inline fun <reified F> Any.junitTests(noinline builder: Context<Unit, F>.() -> Unit): Stream<out DynamicNode> =
-    junitTestsNamed(javaClass.canonicalName, builder)
+    topLevelContext(javaClass.canonicalName, asKType<F>(), builder).toStreamOfDynamicNodes()
 
 inline fun <reified F> Any.junitTests(fixture: F,
     noinline builder: Context<Unit, F>.() -> Unit
 ): Stream<out DynamicNode> =
-    junitTestsNamed(javaClass.canonicalName, fixture, builder)
-
+    topLevelContext(javaClass.canonicalName, asKType<F>(), fixture, builder).toStreamOfDynamicNodes()
 
 // These are defined as extensions to avoid taking a dependency on JUnit in the main package
 
-fun RuntimeNode.toStreamOfDynamicNodes(): Stream<out DynamicNode> =
-    Stream.of(toDynamicNode())
-
-
-fun NodeBuilder<Unit>.toStreamOfDynamicNodes(): Stream<out DynamicNode> =
-    Stream.of(this.toRootRuntimeNode().toDynamicNode())
+fun RuntimeNode.toStreamOfDynamicNodes(): Stream<out DynamicNode> = Stream.of(this.toDynamicNode())
 
 private fun RuntimeNode.toDynamicNode(): DynamicNode = when (this) {
     is RuntimeTest<*> -> dynamicTest(name) { this.run() }
     is RuntimeContext<*, *> -> this.toDynamicContainer()
 }
 
-private fun RuntimeContext<*, *>.toDynamicContainer(): DynamicContainer =
-    dynamicContainer(name, children.map(RuntimeNode::toDynamicNode))
+private fun RuntimeContext<*, *>.toDynamicContainer() = dynamicContainer(name, children.map(RuntimeNode::toDynamicNode))
