4b39ed2abb6cb1726a11d56f8c07800c62856963
Duncan McGregor <duncan@oneeyedmen.com>
2018-11-26 22:25:27 +0000

    Publicise root runtime node building

--- core/src/main/kotlin/com/oneeyedmen/minutest/NodeBuilder.kt
+++ core/src/main/kotlin/com/oneeyedmen/minutest/NodeBuilder.kt
@@ -1,8 +1,11 @@
 package com.oneeyedmen.minutest
 
 import com.oneeyedmen.minutest.internal.ParentContext
+import com.oneeyedmen.minutest.internal.RootContext
 
 interface NodeBuilder<F> {
     val properties: MutableMap<Any, Any>
     fun buildNode(parent: ParentContext<F>): RuntimeNode
 }
+
+fun NodeBuilder<Unit>.buildRootNode(): RuntimeNode = buildNode(RootContext)
--- core/src/main/kotlin/com/oneeyedmen/minutest/internal/creating.kt
+++ core/src/main/kotlin/com/oneeyedmen/minutest/internal/creating.kt
@@ -1,9 +1,9 @@
 package com.oneeyedmen.minutest.internal
 
 import com.oneeyedmen.minutest.Context
-import com.oneeyedmen.minutest.NodeBuilder
 import com.oneeyedmen.minutest.RuntimeNode
 import com.oneeyedmen.minutest.TestDescriptor
+import com.oneeyedmen.minutest.buildRootNode
 import kotlin.reflect.KType
 
 
@@ -13,7 +13,7 @@ fun <F> topLevelContext(
     builder: Context<Unit, F>.() -> Unit
 ): RuntimeNode =
     ContextBuilder<Unit, F>(name, type, fixtureFactoryFor(type), explicitFixtureFactory = false)
-        .apply(builder).toRootRuntimeNode()
+        .apply(builder).buildRootNode()
 
 fun <F> topLevelContext(
     name: String,
@@ -30,11 +30,7 @@ fun <F> topLevelContext(
     builder: Context<Unit, F>.() -> Unit
 ): RuntimeNode =
     ContextBuilder(name, type, fixtureFactory, explicitFixtureFactory = true)
-        .apply(builder).toRootRuntimeNode()
-
-
-internal fun NodeBuilder<Unit>.toRootRuntimeNode(): RuntimeNode =
-    this.buildNode(RootContext)
+        .apply(builder).buildRootNode()
 
 @Suppress("UNCHECKED_CAST")
 private fun <F> fixtureFactoryFor(type: KType): ((Unit, TestDescriptor) -> F)? =
