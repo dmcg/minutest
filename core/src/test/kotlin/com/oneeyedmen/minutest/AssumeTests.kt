67d74ee766c0c41ce5e8deca224414d256947088
Duncan McGregor <duncan@oneeyedmen.com>
2018-11-27 21:46:14 +0000

    Better AssumeTests

--- core/src/test/kotlin/com/oneeyedmen/minutest/AssumeTests.kt
+++ core/src/test/kotlin/com/oneeyedmen/minutest/AssumeTests.kt
@@ -3,6 +3,7 @@ package com.oneeyedmen.minutest
 import com.oneeyedmen.minutest.junit.junitTests
 import org.junit.jupiter.api.Assertions.fail
 import org.junit.jupiter.api.Assumptions.assumeTrue
+import org.junit.jupiter.api.TestFactory
 import org.junit.jupiter.api.assertThrows
 import org.opentest4j.TestAbortedException
 
@@ -21,6 +22,22 @@ class AssumeTests {
         assertThrows<TestAbortedException> {
             executeTest(tests)
         }
+    }
 
+    @TestFactory fun `actually does`() = junitTests<Unit> {
+        test("try it") {
+            assumeTrue("black".toLowerCase() == "white")
+            fail("shouldn't get here")
+        }
+
+        context("works in a fixture block") {
+            modifyFixture {
+                assumeTrue("black".toLowerCase() == "white")
+            }
+            test("try it") {
+                fail("shouldn't get here")
+            }
+
+        }
     }
 }
