8affa373000223c0f5e56587442c646e23cd7f68
Nat Pryce <sw@natpryce.com>
2018-12-10 19:32:10 +0000

    Fixed test data for test engine

--- core/src/test/kotlin/example/b/AnotherMInutestThatShouldBeIgnored.kt
+++ core/src/test/kotlin/example/b/AnotherMInutestThatShouldBeIgnored.kt
@@ -2,7 +2,7 @@ package example.b
 
 import com.oneeyedmen.minutest.experimental.context
 
-val `this context  should not appear in the test plan` = context<Unit> {
+fun `this context  should not appear in the test plan`() = context<Unit> {
     test("this test should not appear in the test plan") {
     
     }
