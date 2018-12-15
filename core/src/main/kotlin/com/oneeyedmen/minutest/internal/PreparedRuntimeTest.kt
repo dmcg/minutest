52c970f80a64c65ba5096c67f186588858adce45
Duncan McGregor <duncan@oneeyedmen.com>
2018-12-15 10:39:28 +0000

    Stop PreparedRuntimeTest being a data class

--- core/src/main/kotlin/com/oneeyedmen/minutest/internal/PreparedRuntimeTest.kt
+++ core/src/main/kotlin/com/oneeyedmen/minutest/internal/PreparedRuntimeTest.kt
@@ -8,16 +8,25 @@ import com.oneeyedmen.minutest.TestDescriptor
 /**
  * The runtime representation of a test.
  */
-internal data class PreparedRuntimeTest<F>(
+internal class PreparedRuntimeTest<F>(
     override val name: String,
-    override val parent: RuntimeContext?,
+    parent: RuntimeContext?,
     private val f: F.(TestDescriptor) -> F,
     override val properties: Map<Any, Any>
-) : RuntimeTest(), Test<F>, (F)-> F {
-    
+) : RuntimeTest(), Test<F>, (F) -> F {
+
+    override val parent: RuntimeContext = parent ?: error("RuntimeTest [$name] must have a parent")
+
     override fun invoke(fixture: F) = fixture.f(this)
     
-    override fun run() = parent!!.runTest(this)
+    override fun run() = parent.runTest(this)
 
     override fun withProperties(properties: Map<Any, Any>) = copy(properties = properties)
+
+    private fun copy(
+        name: String = this.name,
+        parent: RuntimeContext? = this.parent,
+        f:  F.(TestDescriptor) -> F = this.f,
+        properties: Map<Any, Any> = this.properties
+    ) = PreparedRuntimeTest(name, parent, f, properties)
 }
