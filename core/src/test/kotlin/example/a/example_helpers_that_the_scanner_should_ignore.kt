3fbafeeaf6a7d65f97523cd63e423927783d3ee2
Duncan McGregor <duncan@oneeyedmen.com>
2018-12-24 20:40:14 +0000

    Suppress one of the only warnings that I am sure isn't going to bite me

--- core/src/test/kotlin/example/a/example_helpers_that_the_scanner_should_ignore.kt
+++ core/src/test/kotlin/example/a/example_helpers_that_the_scanner_should_ignore.kt
@@ -4,6 +4,7 @@ import com.oneeyedmen.minutest.rootContext
 
 
 // This should not be picked up the the classpath scanner
+@Suppress("unused")
 fun exampleContextHelper(exampleParam: Any) = rootContext<Unit> {
 
 }
