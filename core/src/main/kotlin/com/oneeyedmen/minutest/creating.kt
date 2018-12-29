c0e0ca9cb120351da4ff732f7ed8bc87dcd0675c
Duncan McGregor <duncan@oneeyedmen.com>
2018-12-29 21:39:43 +0000

    KDoc

--- core/src/main/kotlin/com/oneeyedmen/minutest/creating.kt
+++ core/src/main/kotlin/com/oneeyedmen/minutest/creating.kt
@@ -3,7 +3,9 @@ package com.oneeyedmen.minutest
 import com.oneeyedmen.minutest.internal.TopLevelContextBuilder
 import com.oneeyedmen.minutest.internal.askType
 
-
+/**
+ * The entry point to Minutest - defines a context that is not nested within a parent context.
+ */
 inline fun <reified F> rootContext(
     noinline transform: (RuntimeContext<Unit, F>) -> RuntimeContext<Unit, F> = { it },
     name: String = "root",
