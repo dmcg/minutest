c65b321a9294055721e029828ee694a4c3de20f0
Duncan McGregor <duncan@oneeyedmen.com>
2018-12-30 10:28:24 +0000

    Make optional dependencies compileOnly

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -18,11 +18,12 @@ plugins {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-
     implementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
-    implementation("org.junit.vintage:junit-vintage-engine:5.3.2")
-    implementation("junit:junit:4.12")
-    implementation("io.github.classgraph:classgraph:4.6.6")
+
+    // alternative runners - provide your own dependencies
+    compileOnly("org.junit.vintage:junit-vintage-engine:5.3.2")
+    compileOnly("junit:junit:4.12")
+    compileOnly("io.github.classgraph:classgraph:4.6.6")
 
     testCompile(kotlin("test"))
 
