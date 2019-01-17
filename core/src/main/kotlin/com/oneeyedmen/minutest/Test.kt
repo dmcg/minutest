d5c2fed14a6cbb0f6e9180af9d6296b0942b9041
Duncan McGregor <duncan@oneeyedmen.com>
2019-01-16 19:47:16 -0800

    Docs

--- core/src/main/kotlin/com/oneeyedmen/minutest/Test.kt
+++ core/src/main/kotlin/com/oneeyedmen/minutest/Test.kt
@@ -1,6 +1,6 @@
 package com.oneeyedmen.minutest
 
 /**
- * A test that can be invoked on a fixture, with a testDescriptor describing the context.
+ * A test that can be invoked on a fixture, with a [TestDescriptor] describing the context.
  */
 typealias Test<F> = (F, TestDescriptor) -> F
