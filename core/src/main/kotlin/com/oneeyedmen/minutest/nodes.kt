b0e71be475621b516373dbd9195ec690a50afffe
Duncan McGregor <duncan@oneeyedmen.com>
2019-01-31 23:22:44 +0000

    Light tidying

--- core/src/main/kotlin/com/oneeyedmen/minutest/nodes.kt
+++ core/src/main/kotlin/com/oneeyedmen/minutest/nodes.kt
@@ -45,3 +45,13 @@ data class Test<F>(
     
     override fun withTransformedChildren(transform: NodeTransform) = this
 }
+
+interface NodeTransform {
+
+    fun <F> applyTo(node: Node<F>): Node<F>
+
+    fun then(next: (NodeTransform)): NodeTransform = object: NodeTransform {
+        override fun <F> applyTo(node: Node<F>): Node<F> =
+            next.applyTo(this@NodeTransform.applyTo(node))
+    }
+}
