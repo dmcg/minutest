c1429b917495512ce64ec03cbf48316769046692
Duncan McGregor <duncan@oneeyedmen.com>
2019-02-11 09:57:29 +0000

    Tidy up and document TestAnnotation

--- core/src/main/kotlin/dev/minutest/experimental/annotating.kt
+++ core/src/main/kotlin/dev/minutest/experimental/annotating.kt
@@ -4,22 +4,29 @@ import dev.minutest.Node
 import dev.minutest.NodeBuilder
 import dev.minutest.TestContextBuilder
 
+/**
+ * An experimental feature that allows contexts and tests to be annotated in order to change their execution.
+ */
 interface TestAnnotation {
 
-    fun applyTo(nodeBuilder: NodeBuilder<*>) {
-        nodeBuilder.annotations.add(this)
-    }
-
-    fun appliesTo(node: Node<*>) = node.annotations.contains(this)
-
-    operator fun plus(that: TestAnnotation) = listOf(this, that)
-
+    /**
+     * Used (ironically) to *add* an annotation to a context or test block.
+     */
     operator fun <F, NodeBuilderT: NodeBuilder<F>> minus(nodeBuilder: NodeBuilderT): NodeBuilderT =
         nodeBuilder.also {
             this.applyTo(it)
         }
+
+    /**
+     * Groups annotations into a list.
+     */
+    operator fun plus(that: TestAnnotation) = listOf(this, that)
+
 }
 
+/**
+ * Add a list of annotations to a context or test block.
+ */
 operator fun <F, NodeBuilderT: NodeBuilder<F>> Iterable<TestAnnotation>.minus(nodeBuilder: NodeBuilderT): NodeBuilderT=
     nodeBuilder.also {
         this.forEach { annotation ->
@@ -27,6 +34,19 @@ operator fun <F, NodeBuilderT: NodeBuilder<F>> Iterable<TestAnnotation>.minus(no
         }
     }
 
+/**
+ * Adds an annotation to a context block from the inside.
+ */
 fun TestContextBuilder<*, *>.annotateWith(annotation: TestAnnotation) {
     annotation.applyTo(this as NodeBuilder<*>)
+}
+
+/**
+ * For use by annotation transforms to establish if they should.
+ */
+fun TestAnnotation.appliesTo(node: Node<*>) = node.annotations.contains(this)
+
+
+private fun TestAnnotation.applyTo(nodeBuilder: NodeBuilder<*>) {
+    nodeBuilder.annotations.add(this)
 }
