2c41e0588399c2088638db63a61ddd30fb2c485b
Duncan McGregor <duncan@oneeyedmen.com>
2019-02-11 11:58:59 +0000

    Upgrade to JUnit 5.4.0

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -17,19 +17,18 @@ plugins {
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
     implementation(kotlin("reflect"))
-    implementation("org.junit.jupiter:junit-jupiter-api:5.3.2")
+    implementation("org.junit.jupiter:junit-jupiter-api:5.4.0")
     implementation("io.github.classgraph:classgraph:4.6.6")
 
     // alternative runners - provide your own dependencies
-    compileOnly("org.junit.vintage:junit-vintage-engine:5.3.2")
+    compileOnly("org.junit.vintage:junit-vintage-engine:5.4.0")
     compileOnly("junit:junit:4.12")
 
     testCompile(kotlin("test"))
-    testCompile("org.junit.vintage:junit-vintage-engine:5.3.2")
+    testCompile("org.junit.vintage:junit-vintage-engine:5.4.0")
     testCompile("junit:junit:4.12")
-    testCompile("io.github.classgraph:classgraph:4.6.6")
-    testCompile("org.junit.jupiter:junit-jupiter-engine:5.3.2")
-    testCompile("org.junit.platform:junit-platform-launcher:1.3.2") {
+    testCompile("org.junit.jupiter:junit-jupiter-engine:5.4.0")
+    testCompile("org.junit.platform:junit-platform-launcher:1.4.0") {
         because("enables running in IntelliJ using JUnit runner")
     }
 }
