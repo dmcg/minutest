29477a2cccf5aff49125a93d9857d72339d32570
Duncan McGregor <duncan@oneeyedmen.com>
2019-02-13 16:48:54 +0000

    Split reflecting into used and experimental

--- core/src/main/kotlin/dev/minutest/internal/reflecting.kt
+++ core/src/main/kotlin/dev/minutest/internal/reflecting.kt
@@ -1,38 +1,7 @@
 package dev.minutest.internal
 
-import dev.minutest.TestDescriptor
 import kotlin.reflect.KClass
-import kotlin.reflect.KFunction
-import kotlin.reflect.KVisibility
 
 @PublishedApi internal inline fun <reified G> askType() = FixtureType(G::class, null is G)
 
 @PublishedApi internal data class FixtureType(internal val classifier: KClass<*>, internal val isMarkedNullable: Boolean)
-
-internal fun FixtureType.creator(): (() -> Any)? {
-    val classifier = this.classifier
-    if (classifier == Unit::class) return { Unit } // shortcut as we do this a lot
-    val objectInstance = try {
-        classifier.objectInstance
-    } catch (x: Exception) {
-        null
-    }
-    return when {
-        objectInstance != null -> {
-            { objectInstance }
-        }
-        classifier.visibility != KVisibility.PUBLIC -> null
-        else -> classifier.constructors.noArgCtor()?.let { ctor -> { ctor.call() } }
-    }
-}
-
-private fun Collection<KFunction<Any>>.noArgCtor() =
-    find {
-        it.visibility == KVisibility.PUBLIC && it.parameters.isEmpty()
-    }
-
-@Suppress("UNCHECKED_CAST")
-internal fun <F> experimentalFixtureFactoryFor(type: FixtureType): ((Unit, TestDescriptor) -> F)? =
-    type.creator()?.let { creator ->
-        { _: Unit, _: TestDescriptor -> creator() as F }
-    }
--- core/src/test/kotlin/dev/minutest/internal/ReflectingTests.kt
+++ core/src/test/kotlin/dev/minutest/internal/ReflectingTests.kt
@@ -4,7 +4,6 @@ import dev.minutest.junit.JUnit5Minutests
 import dev.minutest.rootContext
 import kotlin.test.assertEquals
 import kotlin.test.assertFalse
-import kotlin.test.assertNull
 import kotlin.test.assertTrue
 
 
@@ -21,48 +20,5 @@ class ReflectingTests : JUnit5Minutests {
                 assertTrue(askType<String?>().isMarkedNullable)
             }
         }
-
-        context("KType.creator") {
-            context("works for") {
-                test("String") {
-                    assertEquals("", askType<String>().creator()!!.invoke())
-                }
-                test("String?") {
-                    assertEquals("", askType<String?>().creator()!!.invoke())
-                }
-                test("Unit") {
-                    assertEquals(Unit, askType<Unit>().creator()!!.invoke())
-                }
-                test("HashMap") {
-                    assertEquals(HashMap<String, String>(), askType<HashMap<String, String>>().creator()!!.invoke())
-                }
-                test("Any") {
-                    assertEquals(Any::class, askType<Any>().creator()!!.invoke()::class)
-                }
-                test("public object") {
-                    assertEquals(PublicObject, askType<PublicObject>().creator()!!.invoke())
-                }
-            }
-            context("is null for") {
-                test("private class") {
-                    assertNull(askType<PrivateClass>().creator())
-                }
-                test("public class with private ctor") {
-                    assertNull(askType<PublicClassPrivateCtor>().creator())
-                }
-                test("private object") {
-                    assertNull(askType<PrivateObject>().creator())
-                }
-            }
-        }
     }
-
 }
-
-private class PrivateClass
-
-private class PublicClassPrivateCtor private constructor()
-
-object PublicObject
-
-private object PrivateObject
