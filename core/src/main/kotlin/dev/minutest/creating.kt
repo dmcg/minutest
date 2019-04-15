c6ba5e6a134f915ea95352a67675d0de966e23a2
Duncan McGregor <duncan@oneeyedmen.com>
2019-04-15 15:41:39 +0100

    Remove unused creating parameters

--- core/src/main/kotlin/dev/minutest/creating.kt
+++ core/src/main/kotlin/dev/minutest/creating.kt
@@ -1,6 +1,5 @@
 package dev.minutest
 
-import dev.minutest.experimental.TestAnnotation
 import dev.minutest.internal.FixtureType
 import dev.minutest.internal.MinutestRootContextBuilder
 import dev.minutest.internal.askType
@@ -8,14 +7,8 @@ import dev.minutest.internal.askType
 /**
  * The entry point to Minutest - defines a context that is not nested within a parent context.
  */
-inline fun <reified F> rootContext(noinline builder: TestContextBuilder<Unit, F>.() -> Unit)
-    : RootContextBuilder<F> = rootContextBuilder("root", askType<F>(), builder)
-
-/**
- * The entry point to Minutest - defines a named context that is not nested within a parent context.
- */
 inline fun <reified F> rootContext(
-    name: String,
+    name: String = "root",
     noinline builder: TestContextBuilder<Unit, F>.() -> Unit
 ): RootContextBuilder<F> = rootContextBuilder(name, askType<F>(), builder)
 
@@ -23,6 +16,5 @@ inline fun <reified F> rootContext(
 @PublishedApi internal fun <F> rootContextBuilder(
     name: String,
     type: FixtureType,
-    builder: TestContextBuilder<Unit, F>.() -> Unit,
-    annotations: MutableList<TestAnnotation<Unit>> = mutableListOf()
-): RootContextBuilder<F> = MinutestRootContextBuilder(name, type, builder, annotations)
+    builder: TestContextBuilder<Unit, F>.() -> Unit
+): RootContextBuilder<F> = MinutestRootContextBuilder(name, type, builder)
