dcb693dfc4b839c3b51b8cf798a32780a864b707
Duncan McGregor <duncan@oneeyedmen.com>
2019-04-16 18:37:35 +0100

    Remove default implementation of TestAnnotation

--- core/src/main/kotlin/dev/minutest/experimental/TestAnnotation.kt
+++ core/src/main/kotlin/dev/minutest/experimental/TestAnnotation.kt
@@ -6,6 +6,11 @@ import dev.minutest.NodeBuilder
  * An experimental feature that allows contexts and tests to be annotated in order to change their execution.
  */
 interface TestAnnotation {
+    fun applyTo(nodeBuilder: NodeBuilder<*>)
+}
 
-    fun applyTo(nodeBuilder: NodeBuilder<*>) = Unit
+class MarkerAnnotation : TestAnnotation {
+    override fun applyTo(nodeBuilder: NodeBuilder<*>) {
+        nodeBuilder.addAnnotation(this)
+    }
 }
