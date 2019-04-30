43c292870ec7db1b4237942121e283c6fb888dba
duncan <duncan@oneeyedmen.com>
2019-04-30 15:29:29 +0100

    Tidy up annotations

--- core/src/main/kotlin/dev/minutest/experimental/annotating.kt
+++ core/src/main/kotlin/dev/minutest/experimental/annotating.kt
@@ -7,7 +7,7 @@ import dev.minutest.TestContextBuilder
 /**
  * Used (ironically) to *add* an annotation to a context or test block.
  */
-operator fun <F, NodeBuilderT: NodeBuilder<F>> TestAnnotation.minus(nodeBuilder: NodeBuilderT): NodeBuilderT =
+operator fun <T: NodeBuilder<*>> TestAnnotation.minus(nodeBuilder: T): T =
     nodeBuilder.also {
         this.applyTo(it)
     }
@@ -20,7 +20,7 @@ operator fun <A: TestAnnotation> A.plus(other: A): List<A> = listOf(this, other)
 /**
  * Add a list of annotations to a context or test block.
  */
-operator fun <F, NodeBuilderT: NodeBuilder<F>> Iterable<TestAnnotation>.minus(nodeBuilder: NodeBuilderT): NodeBuilderT =
+operator fun <T: NodeBuilder<*>> Iterable<TestAnnotation>.minus(nodeBuilder: T): T =
     nodeBuilder.also {
         this.reversed().forEach { annotation ->
             annotation.applyTo(it)
@@ -28,17 +28,10 @@ operator fun <F, NodeBuilderT: NodeBuilder<F>> Iterable<TestAnnotation>.minus(no
     }
 
 /**
- * If you are looking at this you are probably trying to add a mismatched set of [TestAnnotation]s to a test or context
- * block. This definition is provided to try to catch this issue by luring the compiler into failure.
- */
-operator fun <F> Iterable<*>.minus(@Suppress("UNUSED_PARAMETER") thing: NodeBuilder<*>): Nothing = error("")
-
-/**
  * Adds an annotation to a context block from the inside.
  */
 fun <PF, F> TestContextBuilder<PF, F>.annotateWith(annotation: TestAnnotation) {
-    @Suppress("UNCHECKED_CAST") // information hiding downcast
-    annotation.applyTo(this as NodeBuilder<PF>)
+    annotation.applyTo(this as NodeBuilder<*>)
 }
 
 /**
