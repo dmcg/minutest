aea25d8494b89aa0b80e0523173c5add003935be
Duncan McGregor <duncan@oneeyedmen.com>
2019-05-09 12:23:58 +0100

    Simplify creating.kt

--- core/src/main/kotlin/dev/minutest/creating.kt
+++ core/src/main/kotlin/dev/minutest/creating.kt
@@ -3,17 +3,18 @@ package dev.minutest
 import dev.minutest.internal.*
 
 /**
- * The entry point to Minutest without any fixture - defines a context that is not nested within a parent context.
+ * The entry point to Minutest without no fixture type - defines a context that is not nested within a parent context.
  */
 @JvmName("rootContextUnit")
 fun rootContext(
     name: String = "root",
     builder: TestContextBuilder<Unit, Unit>.() -> Unit
-): RootContextBuilder = rootContextBuilder(name, askType<Unit>(), builder, rootFixtureFactory)
-
+): RootContextBuilder = rootContext<Unit>(name, builder)
 
 /**
- * The entry point to Minutest without initial fixture - defines a context that is not nested within a parent context.
+ * The entry point to Minutest with a particular fixture type - defines a context that is not nested within a parent context.
+ *
+ * You must supply a fixture by calling [TestContextBuilder.fixture].
  */
 inline fun <reified F> rootContext(
     name: String = "root",
@@ -25,12 +26,4 @@ internal fun <F> rootWithoutFixture(
     name: String,
     type: FixtureType,
     builder: TestContextBuilder<Unit, F>.() -> Unit
-) = rootContextBuilder(name, type, builder, rootFixtureFactoryHack())
-
-@PublishedApi
-internal fun <F> rootContextBuilder(
-    name: String,
-    type: FixtureType,
-    builder: TestContextBuilder<Unit, F>.() -> Unit,
-    fixtureFactory: FixtureFactory<Unit, F>
-): RootContextBuilder = MinutestRootContextBuilder(MinutestContextBuilder(name, type, fixtureFactory), builder)
+) = MinutestRootContextBuilder(MinutestContextBuilder(name, type, rootFixtureFactoryHack()), builder)
--- core/src/main/kotlin/dev/minutest/internal/FixtureFactory.kt
+++ core/src/main/kotlin/dev/minutest/internal/FixtureFactory.kt
@@ -11,4 +11,4 @@ internal class FixtureFactory<PF, F>(
 @Suppress("UNCHECKED_CAST") // safe as long as we make sure that we fail if the fixture is accessed before it is redefined
 internal fun <PF, F> rootFixtureFactoryHack() = rootFixtureFactory as FixtureFactory<PF, F>
 
-internal val rootFixtureFactory = FixtureFactory<Unit, Unit>(FixtureType(Unit::class, false)) { _, _ -> Unit }
+private val rootFixtureFactory = FixtureFactory<Unit, Unit>(FixtureType(Unit::class, false)) { _, _ -> Unit }
