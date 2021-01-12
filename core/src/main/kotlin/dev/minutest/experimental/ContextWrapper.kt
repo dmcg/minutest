a9fdaf8b40fbeb07e3a46c3938a1512a35596992
Duncan McGregor <duncan.mcgregor@rbs.com>
2021-01-12 13:57:45 +0000

    Documentation and formatting

--- core/src/main/kotlin/dev/minutest/experimental/ContextWrapper.kt
+++ core/src/main/kotlin/dev/minutest/experimental/ContextWrapper.kt
@@ -2,6 +2,13 @@ package dev.minutest.experimental
 
 import dev.minutest.*
 
+/**
+ * A [Context] that can wrap an existing context, delegating specific aspects of behaviour to it
+ * but intercepting or overriding others.
+ *
+ * Used to implement [NodeTransform]s - we don't replace [Context]s in the tree, but instead wrap
+ * them with one of these.
+ */
 internal data class ContextWrapper<PF, F>(
     override val name: String,
     override val markers: List<Any>,
@@ -10,6 +17,10 @@ internal data class ContextWrapper<PF, F>(
     val onClose: () -> Unit
 ) : Context<PF, F>() {
 
+    /**
+     * Builds a wrapper that delegates to an existing context, with the ability to delegate or override
+     * all the things.
+     */
     constructor(
         delegate: Context<PF, F>,
         name: String = delegate.name,
@@ -19,12 +30,14 @@ internal data class ContextWrapper<PF, F>(
         onClose: () -> Unit = delegate::close
     ) : this(name, markers, children, runner, onCloseFor(delegate, onClose))
 
-    override fun runTest(testlet: Testlet<F>, parentFixture: PF, testDescriptor: TestDescriptor): F =
-        runner(testlet, parentFixture, testDescriptor)
+    override fun runTest(
+        testlet: Testlet<F>,
+        parentFixture: PF,
+        testDescriptor: TestDescriptor
+    ): F = runner(testlet, parentFixture, testDescriptor)
 
-    override fun withTransformedChildren(transform: NodeTransform<F>) = copy(
-        children = children.map { transform(it) }
-    )
+    override fun withTransformedChildren(transform: NodeTransform<F>) =
+        copy(children = children.map { transform(it) })
 
     override fun close() = onClose.invoke()
 }
