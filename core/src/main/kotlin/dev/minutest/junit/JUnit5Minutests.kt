8d05450f36ed8d5c9013bd56a769097f23f6f9d3
Duncan McGregor <duncan.mcgregor@rbs.com>
2021-01-13 17:22:48 +0000

    Tidy away JUnit 5 details

--- core/src/main/kotlin/dev/minutest/junit/JUnit5Minutests.kt
+++ core/src/main/kotlin/dev/minutest/junit/JUnit5Minutests.kt
@@ -1,14 +1,9 @@
 package dev.minutest.junit
 
-import dev.minutest.Node
 import dev.minutest.RootContextBuilder
-import dev.minutest.internal.*
-import org.junit.jupiter.api.DynamicContainer.dynamicContainer
+import dev.minutest.internal.rootContextFromMethods
 import org.junit.jupiter.api.DynamicNode
-import org.junit.jupiter.api.DynamicTest.dynamicTest
 import org.junit.jupiter.api.TestFactory
-import java.io.File
-import java.net.URI
 
 /**
  * Mix-in this interface to run your tests with JUnit 5
@@ -39,47 +34,3 @@ fun testFactoryFor(root: RootContextBuilder): Iterable<DynamicNode> =
  */
 fun RootContextBuilder.toTestFactory(): Iterable<DynamicNode> =
     testFactoryFor(this)
-
-// These are defined as extensions to avoid taking a dependency on JUnit in the main package
-
-private fun RunnableNode.toDynamicNode() =
-    when (this) {
-        is RunnableTest -> this.toDynamicTest()
-        is RunnableContext -> this.toDynamicContainer()
-    }
-
-private fun RunnableTest.toDynamicTest() =
-    dynamicTest(name, test.testUri()) {
-        this.invoke()
-    }
-
-private fun RunnableContext.toDynamicContainer() =
-    dynamicContainer(
-        name,
-        context.testUri(),
-        toListOfDynamicNodes().stream()
-    )
-
-private fun Node<Unit>.toRootListOfDynamicNodes(): List<DynamicNode> =
-    when (val runnableNode = this.toRootRunnableNode()) {
-        is RunnableTest -> listOf(runnableNode.toDynamicTest())
-        is RunnableContext -> runnableNode.toListOfDynamicNodes()
-    }
-
-private fun RunnableContext.toListOfDynamicNodes(): List<DynamicNode> =
-    children.map { it.toDynamicNode() }
-
-private fun <F> Node<F>.testUri(): URI? =
-    (this.markers.find { it is SourceReference } as? SourceReference)?.toURI()
-
-private fun SourceReference.toURI(): URI = File(path).toURI().let { fileUri ->
-    URI(
-        fileUri.scheme,
-        fileUri.userInfo,
-        fileUri.host,
-        fileUri.port,
-        "//" + fileUri.path,
-        "line=$lineNumber",
-        fileUri.fragment
-    )
-}
---
+++ core/src/main/kotlin/dev/minutest/junit/junit5.kt
@@ -0,0 +1,53 @@
+package dev.minutest.junit
+
+import dev.minutest.Node
+import dev.minutest.internal.*
+import org.junit.jupiter.api.DynamicContainer
+import org.junit.jupiter.api.DynamicNode
+import org.junit.jupiter.api.DynamicTest
+import java.io.File
+import java.net.URI
+
+// These are defined as extensions to avoid taking a dependency on JUnit in the main package
+
+internal fun Node<Unit>.toRootListOfDynamicNodes(): List<DynamicNode> =
+    when (val runnableNode = this.toRootRunnableNode()) {
+        is RunnableTest -> listOf(runnableNode.toDynamicTest())
+        is RunnableContext -> runnableNode.toListOfDynamicNodes()
+    }
+
+private fun RunnableNode.toDynamicNode() =
+    when (this) {
+        is RunnableTest -> this.toDynamicTest()
+        is RunnableContext -> this.toDynamicContainer()
+    }
+
+private fun RunnableTest.toDynamicTest() =
+    DynamicTest.dynamicTest(name, test.testUri()) {
+        this.invoke()
+    }
+
+private fun RunnableContext.toDynamicContainer() =
+    DynamicContainer.dynamicContainer(
+        name,
+        context.testUri(),
+        toListOfDynamicNodes().stream()
+    )
+
+private fun RunnableContext.toListOfDynamicNodes(): List<DynamicNode> =
+    children.map { it.toDynamicNode() }
+
+private fun <F> Node<F>.testUri(): URI? =
+    (this.markers.find { it is SourceReference } as? SourceReference)?.toURI()
+
+private fun SourceReference.toURI(): URI = File(path).toURI().let { fileUri ->
+    URI(
+        fileUri.scheme,
+        fileUri.userInfo,
+        fileUri.host,
+        fileUri.port,
+        "//" + fileUri.path,
+        "line=$lineNumber",
+        fileUri.fragment
+    )
+}
