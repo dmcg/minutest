864a4ac5c4e22e42183f19a705eace8b404e9067
Duncan McGregor <duncan.mcgregor@rbs.com>
2021-01-19 11:09:31 +0000

    Remove some debug

--- core/src/test/kotlin/dev/minutest/junit/JUnit5ObjectLifecycleTests.kt
+++ core/src/test/kotlin/dev/minutest/junit/JUnit5ObjectLifecycleTests.kt
@@ -16,42 +16,35 @@ private var hashCode: Int = 0
 object JUnit5ObjectLifecycleTests : JUnit5Minutests {
 
     @BeforeAll @JvmStatic fun beforeAll() {
-        println("beforeAll $hashCode ${System.identityHashCode(this@JUnit5ObjectLifecycleTests)}")
         log.add("beforeAll")
     }
 
     @AfterAll @JvmStatic fun afterAll() {
-        println("afterAll $hashCode ${System.identityHashCode(this@JUnit5ObjectLifecycleTests)}")
         log.add("afterAll")
         assertLogged(log, "init", "beforeAll", "beforeEach", "1", "2", "afterEach", "afterAll")
     }
 
     init {
         hashCode = System.identityHashCode(this@JUnit5ObjectLifecycleTests)
-        println("init $hashCode ${System.identityHashCode(this@JUnit5ObjectLifecycleTests)}")
         log.add("init")
     }
 
     @BeforeEach fun beforeEach() {
-        println("beforeEach $hashCode ${System.identityHashCode(this@JUnit5ObjectLifecycleTests)}")
         log.add("beforeEach")
     }
 
     @AfterEach fun afterEach() {
-        println("afterEach $hashCode ${System.identityHashCode(this@JUnit5ObjectLifecycleTests)}")
         log.add("afterEach")
     }
 
     fun tests1() = rootContext {
         test("1") {
-            println("${it.fullName()} $hashCode ${System.identityHashCode(this@JUnit5ObjectLifecycleTests)}")
             log.add("1")
         }
     }
 
     fun tests2() = rootContext {
         test("2") {
-            println("${it.fullName()} $hashCode ${System.identityHashCode(this@JUnit5ObjectLifecycleTests)}")
             log.add("2")
         }
     }
