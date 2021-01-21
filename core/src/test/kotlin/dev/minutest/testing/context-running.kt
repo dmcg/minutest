92bc57d78739751d4adc9e118f356c8ffa7407d9
Duncan McGregor <duncan.mcgregor@rbs.com>
2021-01-21 09:14:45 +0000

    Remove unused

--- core/src/test/kotlin/dev/minutest/testing/context-running.kt
+++ core/src/test/kotlin/dev/minutest/testing/context-running.kt
@@ -5,18 +5,10 @@ import dev.minutest.junit.toTestFactory
 import org.junit.jupiter.api.DynamicContainer
 import org.junit.jupiter.api.DynamicNode
 import org.junit.jupiter.api.DynamicTest
-import org.opentest4j.MultipleFailuresError
 import java.util.stream.Stream
 
 fun runTests(root: RootContextBuilder): List<Throwable> =
     runTests(root.toTestFactory())
-
-fun List<Throwable>.andFailIfTheyFail() {
-    when {
-        this.size == 1 -> throw this[0]
-        this.isNotEmpty() -> throw MultipleFailuresError("tests failed", this)
-    }
-}
 
 private fun runTests(
     nodes: Stream<out DynamicNode>,
