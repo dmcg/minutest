4b49a412d34d78f01ac9eb042d4e45181066d31a
Duncan McGregor <duncan.mcgregor@rbs.com>
2021-01-26 14:32:59 +0000

    Tinidy

--- core/src/test/kotlin/dev/minutest/junit/engine/minutest-runner-test.kt
+++ core/src/test/kotlin/dev/minutest/junit/engine/minutest-runner-test.kt
@@ -2,8 +2,9 @@ package dev.minutest.junit.engine
 
 import dev.minutest.rootContext
 import dev.minutest.test
+import org.junit.platform.commons.annotation.Testable
 
-
+@Testable
 fun `root name`() = rootContext("context name") {
     test("hello") {
 // Uncomment to check the test is being run for now
