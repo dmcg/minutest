ed402fde6431260fd3b3a8b12cfe23bd1d90420f
Duncan McGregor <duncan@oneeyedmen.com>
2021-08-13 17:24:53 +0100

    Record build process

--- release.sh
+++ release.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 set -e
 
-: "${BINTRAY_API_KEY?Need to set BINTRAY_API_KEY}"
-
-./gradlew clean build core:publish core:bintrayUpload --no-build-cache
+./gradlew clean build
+echo Now OSS_PWD=*** ./gradlew clean build publishMavenJavaPublicationToSonatypeStagingRepository -Psigning.password=***
