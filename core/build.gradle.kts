ffb99b19c6beb2fdc816ba8d193856717be0b3bd
Duncan McGregor <duncan@oneeyedmen.com>
2022-09-14 14:47:09 +0100

    Upgrade Hamkrest

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -34,7 +34,7 @@ dependencies {
 
     // for examples
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.3.3")
-    testImplementation("com.natpryce:hamkrest:1.7.0.0")
+    testImplementation("com.natpryce:hamkrest:1.8.0.1")
     testImplementation("com.oneeyedmen:k-sera:1.0.0")
 }
 
